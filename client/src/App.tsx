7b134576a03d5bbbd10d32254485b9fb570e4867
cheraa-git <chernetsov.aa@mail.ru>
2023-01-31 18:45:50 +0400

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mt-5">dHello world</h1>
+    </>
+  )
+}
+
+export default App
