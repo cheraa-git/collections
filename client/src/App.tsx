6a7f5279de21f78f94e316657b25b87265c17e20
cheraa-git <chernetsov.aa@mail.ru>
2023-01-31 18:46:37 +0400

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold text-center mt-5">dHello world</h1>
+      <h1 className="text-2xl font-bold text-center mt-5">Hello world</h1>
     </>
   )
 }
